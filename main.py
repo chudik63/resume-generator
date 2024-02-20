2db3e61ad28114fdbaf30cdb734c5cc3ac50bed9
Vitalya <vitamin11182005@gmail.com>
2024-02-20 13:59:30 +0300

    Edit main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 def main():
 	with open('resume.txt', 'w') as f:
-	f.write('Имя: Винт')
-	if __name__ == "__main__":
-main()
+		f.write('Имя: Винт')
+		f.write('Место работы: МИЭМ НИУ ВШЭ')
+
+if __name__ == "__main__":
+	main()
