17b44a75e8f2731a36085f1b40498ee2761abe62
Vitalya <vitamin11182005@gmail.com>
2024-03-07 17:33:54 +0300

    Make main.go work with public folder

--- main.go
+++ main.go
@@ -1,23 +1,18 @@
 package main
 
 import (
-	"fmt"
 	"homework/resume_manipulator"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", print_resume)
-	err := http.ListenAndServe(":8080", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
-func print_resume(w http.ResponseWriter, _ *http.Request) {
 	resume_manipulator.GenerateResume()
-	_, err := fmt.Fprintf(w, string(resume_manipulator.GetResume()))
+	fs := http.FileServer(http.Dir("public"))
+	http.Handle("/", fs)
+
+	log.Print("Listening on :8080...")
+	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
