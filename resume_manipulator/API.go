1db77b5c85e96faf6b478b77621ee545b6f2f9a5
Vitalya <vitamin11182005@gmail.com>
2024-03-08 02:08:37 +0300

    Add checking if the fields is nil

--- resume_manipulator/API.go
+++ resume_manipulator/API.go
@@ -42,17 +42,23 @@ func GenerateResume() {
 
 	data := message["response"][0]
 	career := data["career"].([]interface{})[0].(map[string]interface{})
-	info := Content{
-		FirstName:  data["first_name"].(string),
-		LastName:   data["last_name"].(string),
-		City:       data["city"].(map[string]interface{})["title"].(string),
-		University: data["university_name"].(string),
-		Faculty:    data["faculty_name"].(string),
-		Graduation: data["graduation"].(float64),
-		Experience: Career{Company: career["company"].(string), From: career["from"].(float64), Until: career["until"].(float64), Position: career["position"].(string)},
-		BirthDate:  data["bdate"].(string),
-		DateTime:   time.Now().Format("02.01.2006 15:04"),
+	info := Content{}
+	info.FirstName = data["first_name"].(string)
+	info.LastName = data["last_name"].(string)
+	info.BirthDate = data["bdate"].(string)
+	if city := data["city"]; city != nil {
+		info.City = city.(map[string]interface{})["title"].(string)
 	}
+	if uni := data["university_name"]; uni != nil {
+		info.University = uni.(string)
+		info.Faculty = data["faculty_name"].(string)
+		info.Graduation = data["graduation"].(float64)
+	}
+	if comp := career["company"]; comp != nil {
+		info.Experience = Career{Company: comp.(string), From: career["from"].(float64), Until: career["until"].(float64), Position: career["position"].(string)}
+	}
+	info.DateTime = time.Now().Format("02.01.2006 15:04")
+
 	tmpl, _ := template.ParseFiles(TemplateFileName)
 	publicFile, err := os.OpenFile(PublicFileName, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
